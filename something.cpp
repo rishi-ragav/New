485ec0d18cff386f3284e8966ee02c80a1c11fb4
Rishi Ragav <rishiragav1@gmail.com>
2021-03-19 20:31:26 +0530

    init commit

---
+++ something.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a[10],n;
+    cout<<"the number of values are"<<endl;
+   cin>>n;
+    for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            i++;
+        }
+    for(int i=0;i<n;i++)
+        {
+            cout<<a[i]<<endl;
+            i++;
+        }
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+cout<<"hello world";
+return 0;
+//getch();
+}
